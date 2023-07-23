144144cb57ea493bf95e09cdab294a7fdc678258
scan119 <scan911@naver.com>
2023-07-23 20:39:22 +0900

    최초 커밋

---
+++ src/test/java/codesuda/inLine/InLineApplicationTests.java
@@ -0,0 +1,13 @@
+package codesuda.inLine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
